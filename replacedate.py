db5b96a86662eb38b8870916e742693d7a3be115
lepsalex <lepsalex@gmail.com>
2016-02-19 13:46:54 -0500

    Updated to be more generalized and included sample data

--- replacedate.py
+++ replacedate.py
@@ -1,25 +1,28 @@
+# Search through json file to replace formatted dates with UNIX timestamps
 import time
 import datetime
 
 # Read in the file
-file = open('data.json', 'r')
-filedata = file.read()
+file = open('file.json', 'r')
+# Create temporary file buffer to manipulate
+fileData = file.read()
 file.close()
 
-# Generate replacement array
-query = [];
-for line in open('data.json'):
+# Go through the file line-by-line
+for line in open('file.json'):
+    # If the query string is present in the line
     if "startTime" in line:
-        s = line.strip()
-        t = s[14:-2]
-        query.append(t)
+        # Get the line and strip empty space
+        lineString = line.strip()
+        # Slice the line for the current date length
+        timeString = lineString[14:-2]
+        # Convert the current formatted time string to UNIX datestamp format
+        timeStamp = time.mktime(datetime.datetime.strptime(timeString, "%Y-%m-%dT%H:%M:%S.000Z").timetuple())
+        # Replace the date
+        fileData = fileData.replace(timeString, str(timeStamp))
 
-# Replace the target string
-for replacement in query:
-    timestamp = time.mktime(datetime.datetime.strptime(replacement, "%Y-%m-%dT%H:%M:%S.000Z").timetuple())
-    filedata = filedata.replace(replacement, str(timestamp))
-
-# Write the file out again
-f = open('data.json','w')
-f.write(filedata)
+# Read in file for write this time
+f = open('file-converted.json','w')
+# Write filedata (buffer) to file
+f.write(fileData)
 f.close()
